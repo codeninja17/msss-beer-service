83f746d9a01a532ef7587b2d0a1e36d4527106b0
codeninja17 <ankitkr09@gmail.com>
2021-04-04 20:41:55 +0530

    Initial Setup

---
+++ src/test/java/com/mssc/brewery/beerservice/BeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mssc.brewery.beerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
